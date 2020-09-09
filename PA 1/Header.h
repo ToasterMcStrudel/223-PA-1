7a16adc9aafb39061a541f73920d3e514be0fe7c
ToasterMcStrudel <ryan.j.ortuno@gmail.com>
2020-09-09 09:33:51 -0700

    populated list

--- PA 1/Header.h
+++ PA 1/Header.h
@@ -1,6 +1,9 @@
 #pragma once
-#include<iostream>
-#include<string>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+
 
 using namespace std;
 
@@ -11,53 +14,183 @@ private:
 public:
 
 	Data(string com, string des) { //Just reads lines from csv file
-		this->command = com;
-		this->description = des;
+		command = com;
+		description = des;
 	}
+
+	string getCommand() {
+		return this->command;
+	}
+
+	string getDescription() {
+		return this->description;
+	}
+
+	string setCommand(string com) {
+		return this->command = com;
+	}
+	string setDesription(string des) {
+		return this->description = des;
+	}
+	
+
 };
 
-template<class DATATYPE>  class Node {
+template<class DATATYPE>
+class Node {
 private:
-	DATATYPE* var = nullptr;
+	DATATYPE* data = nullptr;
 	Node* next = nullptr;
+	int ID = NULL;
 
 public:
 	
 	Node(string com, string des) { 
-		this->var = new DATATYPE(com, des);
+	
+		
+		data = new DATATYPE(com,des);
+
 	}
 
-	Node* getNextPointer() {
+	//Destructor;
+
+	//Setters and getters;
+	///////////////////////////////
+	
+	void setData(DATATYPE newData) {
+		this->data = newData;
+	}
+	void setNext(Node* newNext) {
+		this->next = newNext;
+	}
+	void setID(int newID) {
+		this->ID = newID;
+	}
+	DATATYPE* getData() {
+		return this->data;
+	}
+	
+	Node* getNext() {
 		return this->next;
 	}
-
-	void setNextPointer(Node* newTail ) {
-		next = newTail;
+	int getID() {
+		return this->ID;
 	}
+		
+	string getCommand() {
+		return this->data->getCommand();
+	}
+	string getDescription() {
+		return this->data->getDescription();
+	}
+	
 };
 
-template<class NODETYPE> class List {
+template<class NODETYPE>
+class List {
 private:
-	NODETYPE* head;
+	NODETYPE* head = nullptr;
+	string name = "";
+	int score = NULL;
 	
 public:
 
 	List() { //Links nodes together
 		//Populate linked list
-		//open file
-		//while !eof {
-			//read line
-			//parse data
-			//create Node with data as parameters
-			//add to list }
+		ifstream infile;
+		infile.open("commands.csv", ios::in);
+		while (!infile.eof()) {
+			string token;
+			getline(infile, token, '$');
+			string com = token;
+			getline(infile, token, '$');
+			string des = token;
+			//cout << com << endl << des << endl;
+			if (com != "" && des != "") {
+				NODETYPE* data = new NODETYPE(com, des);
+				insertFront(data);
+			}
+		}
+		infile.close();
+	
 	}
+	/**********************************/
+	/*            SETTERS			  */
+	/**********************************/
+	void setHead(NODETYPE* newHead) {
+		this->head = newHead;
+	}
+	void setName(string newName) {
+		this->name = newName;
+	}
+	void setScore(int newScore) {
+		this->score;
+	}
+	/**********************************/
+	/*            GETTERS			  */
+	/**********************************/
+	NODETYPE* getHead() {
+		return this->head;
+	}
+	string getName() {
+		return this->name;
+	}
+	int getScore() {
+		return this->score;
+	}
+
+	// Allocates a new node with given data
+	NODETYPE* newNode(NODETYPE * newData)
+	{
+		this->setHead(newData);
+		newData->setNext(NULL);
+		return newData;
+	}
+
 	//Destructor
+	void insertFront(NODETYPE * newData)
+	{
+		if (!this->getHead()) {
+			return this->setHead(newData);
+		}
 
-	NODETYPE* insertAtFront() {
-
+		NODETYPE* tempHead = this->getHead();
+		this->setHead(newData);
+		this->getHead()->setNext(tempHead);
 	}
-	NODETYPE* removeNode() {
 
+	/*NODETYPE* removeNode() {
+
+	}*/
+
+	/*********************************************
+	Int input validation check and buffer clear. *
+	**********************************************/
+	int checkNUM() {
+		int num;
+		while (!(cin >> num)) {
+			cout << "ERROR: a number must be entered: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		return num;
+	}
+
+	void printList() {
+		
+		if (!this->getHead()) {
+			cout << "There is no list stupid" << endl;
+		}
+		else {
+			NODETYPE* temp = this->getHead();
+			while (temp) {
+				cout << "Command is " << temp->getCommand() << endl;
+				cout << "Description is " << temp->getDescription() << endl;
+				temp = temp->getNext();
+			}
+		}
+
+		
 	}
 
 };
--- PA 1/main.cpp
+++ PA 1/main.cpp
@@ -7,6 +7,8 @@
 int main() {
 
 	List<Node<Data>>* temp = new List<Node<Data>>;
+	temp->printList();
+	cout << "\npie" << endl;
 
 
 }
